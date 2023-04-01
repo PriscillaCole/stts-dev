764720b9767a232384d603f42916f15a1389aacc
PriscillaCole <amokolpriscilla@gmail.com>
2023-04-02 02:03:23 +0300

    notifications

--- app/Models/SubGrower.php
+++ app/Models/SubGrower.php
@@ -100,19 +100,12 @@ class SubGrower extends Model
         });
 
         self::created(function ($model) {
-            $not = new MyNotification();
-            $not->role_id = 2; 
-            $not->message = 'New Sub-grower form has been added by '.Admin::user()->name.' '; 
-            $not->link = admin_url("sub-growers/{$model->id}"); 
-            $not->status = 'Unread'; 
-            $not->model = 'SubGrower';
-            $not->model_id = $model->id; 
-            $not->group_type = 'Group'; 
-            $not->action_status_to_make_done = '[]'; 
-            $not->save();
 
-            //created
+            Utils::send_notification($model, 'SubGrower', request()->segment(count(request()->segments())));
+               
         });
+        
+
 
         self::updating(function ($sr10) {
             //$sr10->status = 16;
@@ -121,100 +114,7 @@ class SubGrower extends Model
         self::updated(function ($sr10) {
 
             // ... code here
-            $notifications = MyNotification::where('model', 'SubGrower')
-            ->where('model_id', $sr10->id) 
-            ->get();
-            foreach($notifications as $n){ 
-                $n->delete();
-            }
- 
-            //assigned status
-            if($sr10->status == 2){
-                $inspector  = Administrator::find($sr10->inspector);
-                if($inspector != null){
-                    $not = new MyNotification();
-                    $not->receiver_id = $inspector->id; 
-                    $not->message = "Dear {$inspector->name}, you have been assigned to inspect Sub-grower form #{$sr10->id}."; 
-                    $not->link = admin_url("sub-growers/{$sr10->id}"); 
-                    $not->status = 'Unread'; 
-                    $not->model = 'SubGrower';
-                    $not->model_id = $sr10->id; 
-                    $not->group_type = 'Individual'; 
-                    $not->action_status_to_make_done = '[]'; 
-                    $not->save();  
-                } 
-                $farmer  = Administrator::find($sr10->administrator_id);
-                if($farmer != null){
-                    $not = new MyNotification();
-                    $not->receiver_id = $farmer->id; 
-                    $not->role_id = 3;
-                    $not->message = "Dear {$farmer->name}, your Sub-grower form #{$sr10->id} is now under inspection."; 
-                    $not->link = admin_url("sub-growers/{$sr10->id}"); 
-                    $not->status = 'Unread'; 
-                    $not->model = 'SubGrower';
-                    $not->model_id = $sr10->id; 
-                    $not->group_type = 'Individual'; 
-                    $not->action_status_to_make_done = '[]'; 
-                    $not->save();  
-                }
-            }
-
-            //halted status for farmer
-            if($sr10->status == 3){
-                $farmer  = Administrator::find($sr10->administrator_id);
-                if($farmer != null){
-                    $not = new MyNotification();
-                    $not->receiver_id = $farmer->id; 
-                    $not->role_id = 3; 
-                    $not->message = "Dear {$farmer->name}, your Sub-grower form #{$sr10->id} has been halted by the inspector."; 
-                    $not->link = admin_url("sub-growers/{$sr10->id}"); 
-                    $not->status = 'Unread'; 
-                    $not->model = 'SubGrower';
-                    $not->model_id = $sr10->id; 
-                    $not->group_type = 'Individual'; 
-                    $not->action_status_to_make_done = '[]'; 
-                    $not->save();  
-                }
-            }
-
-            //rejected status for farmer
-            if($sr10->status == 4){
-                $farmer  = Administrator::find($sr10->administrator_id);
-                if($farmer != null){
-                    $not = new MyNotification();
-                    $not->receiver_id = $farmer->id; 
-                    $not->role_id = 3;
-                    $not->message = "Dear {$farmer->name}, your Sub-grower form #{$sr10->id} has been rejected by the inspector."; 
-                    $not->link = admin_url("sub-growers/{$sr10->id}"); 
-                    $not->status = 'Unread'; 
-                    $not->model = 'SubGrower';
-                    $not->model_id = $sr10->id; 
-                    $not->group_type = 'Individual'; 
-                    $not->action_status_to_make_done = '[]'; 
-                    $not->save();  
-                }
-            }
-
-            //approved status for farmer
-            if($sr10->status == 16){
-                $farmer  = Administrator::find($sr10->administrator_id);
-                if($farmer != null){
-                    $not = new MyNotification();
-                    $not->receiver_id = $farmer->id;
-                    $not->role_id = 3; 
-                    $not->message = "Dear {$farmer->name}, your Sub-grower form #{$sr10->id}/n has been initailized by the inspector."; 
-                    $not->link = admin_url("sub-growers/{$sr10->id}"); 
-                    $not->status = 'Unread'; 
-                    $not->model = 'SubGrower';
-                    $not->model_id = $sr10->id; 
-                    $not->group_type = 'Individual_i'; 
-                    $not->action_status_to_make_done = '[]'; 
-                    $not->save(); 
-                    
-                    self::sendMail($not);
-                }
-            }
-
+            Utils::update_notification($sr10, 'SubGrower', request()->segment(count(request()->segments())-1));
 
             if (Admin::user()->isRole('inspector')) {
                 $id = (int)($sr10->crop);
